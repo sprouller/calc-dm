0f6dd21cb378a7c8f263706b3e7d4009155c459e
JoshC-A <josh@spydr.com>
2024-06-12 17:10:00 +0100

    update tax bands for fy24

--- index.js
+++ index.js
@@ -1,147 +1,106 @@
 // Fully working Version.
-$(document).ready(function(){
-
+$(document).ready(function () {
   // Add comma to input field
-    $('#income').keyup(function(event) {
-
+  $("#income").keyup(function (event) {
     // Skip for arrow keys controller
-    if(event.which >= 37 && event.which <= 40) return;
+    if (event.which >= 37 && event.which <= 40) return;
 
     // Format number input to show commas and prefix £
-    $(this).val(function(index, value) {
-      return "£" + value
-      .replace(/\D/g, "")
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-      ;
+    $(this).val(function (index, value) {
+      return (
+        "£" + value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      );
     });
   });
 
+  $("#monthly-saving").attr({
+    max: 150, // Max number input val in monthly
+    min: 10, // Min number input val in monthly
+  });
 
-$('#monthly-saving').attr({
-       "max" : 150,        // Max number input val in monthly
-       "min" : 10          // Min number input val in monthly
-    });
-
-// On click of calculate button
-$("#dmGo").click(function(){
-
-  // Get user inputs from form fields
+  // On click of calculate button
+  $("#dmGo").click(function () {
+    // Get user inputs from form fields
     let savingPeriod = $("input[name='period']:checked").val();
-    let monthlySaving =  Number($('#monthly-saving').val());
+    let monthlySaving = Number($("#monthly-saving").val());
     let dmValue = monthlySaving * 2;
 
-    let getIncome =  $("#income").val().split(",").join("");
-    let totalIncome = getIncome.replace(/\D/g, '');
+    let getIncome = $("#income").val().split(",").join("");
+    let totalIncome = getIncome.replace(/\D/g, "");
 
     let incomeMin = 12570;
 
-  if($('#monthly-saving').val()<10 || $('#monthly-saving').val()>150 ){
-      $('#savingError').show();
-      $('#result').hide();
-
-    }
-    else{
-      $('#savingError').hide();
-      $('#result').show();
+    if ($("#monthly-saving").val() < 10 || $("#monthly-saving").val() > 150) {
+      $("#savingError").show();
+      $("#result").hide();
+    } else {
+      $("#savingError").hide();
+      $("#result").show();
     }
 
-    
-    if(totalIncome < incomeMin){
-      $('#incomeMin').show();
+    if (totalIncome < incomeMin) {
+      $("#incomeMin").show();
+    } else {
+      $("#incomeMin").hide();
+      $("#result").show();
+    }
 
-  }
- 
-      else{
-        $('#incomeMin').hide();
-        $('#result').show();
+    //Tax Bands
+    let eng0 = 0;
+    let eng1 = 30;
+    let eng2 = 42;
+    let eng3 = 47;
+
+    let scot0 = 0;
+    let scot1 = 29;
+    let scot2 = 30;
+    let scot3 = 31;
+    let scot4 = 52;
+    let scot5 = 44;
+    let scot6 = 48;
+
+    // If England selected as
+    if (savingPeriod == "eng") {
+      if (totalIncome > 150000) {
+        taxCalc = monthlySaving * (1 - eng3 / 100);
+      } else if (totalIncome > 50270 && totalIncome <= 150000) {
+        taxCalc = monthlySaving * (1 - eng2 / 100);
+      } else if (totalIncome > 12570 && totalIncome <= 50270) {
+        taxCalc = monthlySaving * (1 - eng1 / 100);
+      } else if (totalIncome <= 12570) {
+        taxCalc = monthlySaving;
+      } else {
+        taxCalc = "Error";
       }
-
-
-
-  //Tax Bands
-  let eng0 = 0;
-  let eng1 = 32;
-  let eng2 = 42;
-  let eng3 = 47;
-
-  let scot0 = 0;
-  let scot1 = 32;
-  let scot2 = 32;
-  let scot3 = 33;
-  let scot4 = 53;
-  let scot5 = 43;
-  let scot6 = 48;
-
-  // If England selected as 
-  if (savingPeriod == "eng") {
-
-    if (totalIncome > 150000){
-      taxCalc = monthlySaving*(1 - (eng3 / 100));
+    } else if (savingPeriod == "scot") {
+      if (totalIncome > 150000) {
+        taxCalc = monthlySaving * (1 - scot6 / 100);
+      } else if (totalIncome > 50270 && totalIncome <= 150000) {
+        taxCalc = monthlySaving * (1 - scot5 / 100);
+      } else if (totalIncome > 43662 && totalIncome <= 50270) {
+        taxCalc = monthlySaving * (1 - scot4 / 100);
+      } else if (totalIncome > 25688 && totalIncome <= 43662) {
+        taxCalc = monthlySaving * (1 - scot3 / 100);
+      } else if (totalIncome > 14732 && totalIncome <= 25688) {
+        taxCalc = monthlySaving * (1 - scot2 / 100);
+      } else if (totalIncome > 12570 && totalIncome <= 14732) {
+        taxCalc = monthlySaving * (1 - scot1 / 100);
+      } else if (totalIncome <= 12570) {
+        taxCalc = monthlySaving;
+      } else {
+        taxCalc = "Error";
+      }
     }
-
-    else if (totalIncome > 50270 && totalIncome <= 150000) {
-      taxCalc = monthlySaving*(1 - (eng2 / 100));
-    }
-
-    else if (totalIncome > 12570 && totalIncome <= 50270) {
-      taxCalc = monthlySaving*(1 - (eng1 / 100));
-    }
-
-    else if (totalIncome <= 12570) {
-      taxCalc = monthlySaving;
-    }
-
-    else {
-      taxCalc = "Error";
-    }
-
-  }
-
-  else if (savingPeriod == "scot") {
-
-    if (totalIncome > 150000){
-      taxCalc = monthlySaving*(1 - (scot6 / 100));
-    }
-
-    else if (totalIncome > 50270 && totalIncome <= 150000) {
-      taxCalc = monthlySaving*(1 - (scot5 / 100));
-    }
-
-    else if (totalIncome > 43662 && totalIncome <= 50270) {
-      taxCalc = monthlySaving*(1 - (scot4 / 100));
-    }
-
-    else if (totalIncome > 25688 && totalIncome <= 43662) {
-      taxCalc = monthlySaving*(1 - (scot3 / 100));
-    }
-
-    else if (totalIncome > 14732 && totalIncome <= 25688) {
-      taxCalc = monthlySaving*(1 - (scot2 / 100));
-    }
-
-    else if (totalIncome > 12570 && totalIncome <= 14732) {
-      taxCalc = monthlySaving*(1 - (scot1 / 100));
-    }
-
-    else if (totalIncome <= 12570) {
-      taxCalc = monthlySaving;
-    }
-
-    else {
-      taxCalc = "Error";
-    }
-
-  }
 
     let taxCalcRnd = Math.round(taxCalc * 100) / 100;
 
-  // update shares1
-  $('#dmShareCost').text(formatNumber(taxCalcRnd));
-  // update share value
-  $('#dmShareValue').text(formatNumber(dmValue));
-});
+    // update shares1
+    $("#dmShareCost").text(formatNumber(taxCalcRnd));
+    // update share value
+    $("#dmShareValue").text(formatNumber(dmValue));
+  });
 
-function formatNumber(num){
-  return new Intl.NumberFormat().format(num);
-}
+  function formatNumber(num) {
+    return new Intl.NumberFormat().format(num);
+  }
 });
